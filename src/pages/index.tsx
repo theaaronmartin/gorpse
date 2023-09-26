1b60fe2c3beea876202f252d235973f1237748df
theaaronmartin <grav3serker@gmail.com>
2023-09-25 19:13:53 -0700

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import type { HeadFC, PageProps } from "gatsby";
+import logo from "../images/gorpse-logo.png";
+
+const IndexPage: React.FC<PageProps> = () => {
+  return (
+    <main className="bg-black min-h-screen">
+      <h1>Welcome to Gorpse</h1>
+      <div>
+        <a
+          className="flex justify-center items-center"
+          href="https://gorpse.bandcamp.com"
+        >
+          <img src={logo} className="bg-cover h-3/6 w-4/6" />
+        </a>
+      </div>
+    </main>
+  );
+};
+
+export default IndexPage;
+
+export const Head: HeadFC = () => <title>Gorpse</title>;
