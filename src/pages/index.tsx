73c1824c0b378cef39beafaaf8142222cb26cd67
theaaronmartin <grav3serker@gmail.com>
2023-09-26 11:40:26 -0700

    cleaning up css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,17 +4,14 @@ import logo from "../images/gorpse-logo.png";
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <main className="bg-black min-h-screen">
-      <h1>Welcome to Gorpse</h1>
-      <div>
-        <a
-          className="flex justify-center items-center"
-          href="https://gorpse.bandcamp.com"
-          target="_self"
-        >
-          <img src={logo} className="bg-cover h-3/6 w-4/6" />
-        </a>
-      </div>
+    <main className="flex flex-col justify-center bg-black min-h-screen ">
+      <a
+        className="flex justify-center"
+        href="https://gorpse.bandcamp.com"
+        target="_self"
+      >
+        <img src={logo} className="bg-cover h-3/6 w-4/6" />
+      </a>
     </main>
   );
 };
