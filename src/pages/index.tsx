e0bd7940d29bf466f5c4f6bc3158e83a042fdf95
theaaronmartin <grav3serker@gmail.com>
2023-09-25 21:21:26 -0700

    testing redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,14 +6,14 @@ const IndexPage: React.FC<PageProps> = () => {
   return (
     <main className="bg-black min-h-screen">
       <h1>Welcome to Gorpse</h1>
-      <div>
-        <a
+      <div className="flex justify-center items-center">
+        {/* <a
           className="flex justify-center items-center"
           href="https://gorpse.bandcamp.com"
           target="_self"
-        >
-          <img src={logo} className="bg-cover h-3/6 w-4/6" />
-        </a>
+        > */}
+        <img src={logo} className="bg-cover h-3/6 w-4/6" />
+        {/* </a> */}
       </div>
     </main>
   );
